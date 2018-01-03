d15ff3fa9a7fde0f764f872baa38c4ce6c720f2b
Emilie Dussaix <emiliedussaix@Emilies-Air.home>
2018-01-03 15:46:09 +0000

    try to make the test fail

--- src/.test.js
+++ src/.test.js
@@ -6,7 +6,7 @@ test('render Start', () => {
   const wrapper = shallow(
     <App title='mockTitle' />
   );
-  expect(wrapper.state().startButton).toEqual('Start');
+  expect(wrapper.state().startButton).toEqual('xxx');
   // wrapper.simulate('click');
   // expect(wrapper.state().startButton).toEqual('Next Challenge');
 });
